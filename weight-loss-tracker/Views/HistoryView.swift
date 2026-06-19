e2b626c403f0888510de5b28af5f7107215ef0e7
sherary <sherary.apriliana@gmail.com>
2026-06-19 21:00:03 +0700

    FEAT: add view + view controller for history tab bar | add filter by week range | add subview for text carousel

--- weight-loss-tracker/Views/HistoryView.swift
+++ weight-loss-tracker/Views/HistoryView.swift
@@ -8,35 +8,62 @@
 import UIKit
 
 final class HistoryView: UIView {
+    private let padding: CGFloat = 8
+    
     internal let weightHistoryTable: UITableView = UITableView(frame: .zero, style: .plain)
     internal let addButtonItem: UIBarButtonItem = UIBarButtonItem()
+    internal let segmentedControl: UISegmentedControl = UISegmentedControl(items: ["Weeks", "Months", "Year"])
+    internal var onTappedToVC: ((Int) -> Void)?
+    
+    private lazy var titleLbl: TextCarouselView = TextCarouselView()
+    
+    internal var text: String = "Title" {
+        didSet {
+            titleLbl.text = text
+        }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         setupLayout()
+        
+        titleLbl.onTapped = { [weak self] action in
+            self?.onTappedToVC?(action)
+        }
     }
     
     required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        
-        setupLayout()
+        fatalError()
     }
     
     private func setupLayout() {
         self.backgroundColor = .systemBackground
         self.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 20, leading: 16, bottom: 20, trailing: 16)
 
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        titleLbl.translatesAutoresizingMaskIntoConstraints = false
+        
         weightHistoryTable.translatesAutoresizingMaskIntoConstraints = false
         weightHistoryTable.setContentHuggingPriority(.defaultLow, for: .vertical)
         
-        self.addSubview(weightHistoryTable)
+        addSubview(segmentedControl)
+        addSubview(titleLbl)
+        addSubview(weightHistoryTable)
         
         NSLayoutConstraint.activate([
-            weightHistoryTable.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor),
-            weightHistoryTable.leadingAnchor.constraint(equalTo: self.layoutMarginsGuide.leadingAnchor),
-            weightHistoryTable.trailingAnchor.constraint(equalTo: self.layoutMarginsGuide.trailingAnchor),
-            weightHistoryTable.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor)
+            segmentedControl.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+            segmentedControl.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            segmentedControl.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            
+            titleLbl.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: padding),
+            titleLbl.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            titleLbl.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            
+            weightHistoryTable.topAnchor.constraint(equalTo: titleLbl.bottomAnchor, constant: padding),
+            weightHistoryTable.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            weightHistoryTable.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            weightHistoryTable.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 }
